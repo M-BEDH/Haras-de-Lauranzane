9e62fce97e4e1532ae806d8088ba985fcfab0bd7
M-BEDH <mel.b.ms@pm.me>
2024-11-18 15:55:28 +0100

    location page etalon

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     images.forEach(function(image) {
         image.addEventListener('click', function() {
             // Action à effectuer lors du clic sur l'image
-            // alert('Image cliquée : vous avez choisie l\'Etalon ' + image.src);
+            alert('Image cliquée : vous avez choisie l\'Etalon ' + image.src);
 
             // redirigé vers la page de l'etalon en fonction de l'image cliquée
             if (image.src == 'http://127.0.0.1:5500/assets/images/etalon1.webp') {
